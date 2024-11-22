2c10769e64ce1ec4a8532eb0304ecbadf5f1eee3
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-11-22 13:00:47 +0300

    commit

---
+++ 8kyu/Beginner - Lost Without a Map/index.js
@@ -0,0 +1,19 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//SOLUTION
+
+// function maps(x) {
+//     let doubleX = [];
+//     for (let i = 0; i < x.length; i++) {
+//         doubleX.push(x[i] * 2);
+//     }
+//     return doubleX
+// }
+
+function maps(x) {
+    return x.map(item => item * 2);
+}
